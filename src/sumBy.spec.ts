4528dde6985923ef379e8ca6d182ed0c4ba6ac99
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2022-09-14 17:50:10 +0800

    test: coverage

--- src/sumBy.spec.ts
+++ src/sumBy.spec.ts
@@ -4,4 +4,10 @@ describe('sumBy()', () => {
   it('sumBy decimal numbers with string path', () => {
     expect(sumBy([{ foo: 0.1 }, { foo: 0.2 }], 'foo')).toBe(0.3);
   });
+
+  it('sumBy decimal numbers with function selector', () => {
+    expect(sumBy([{ foo: 0.1 }, { foo: 0.2 }], (item: any) => item.foo)).toBe(
+      0.3
+    );
+  });
 });
